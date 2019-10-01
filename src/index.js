fe9cf0e41c08afafb3741ae949fd85711f1863fb
Daniel Vassallo <dvassallo83@gmail.com>
2019-09-30 18:41:39 -0700

    Migrate JS to ES6

--- src/index.js
+++ src/index.js
@@ -1,32 +1,14 @@
 import './style.css'
 
-var serialize = function (form) {
-  var serialized = ''
-
-  for (var i = 0; i < form.elements.length; i++) {
-
-    var field = form.elements[i]
-
-    if (!field.name || field.disabled || field.type === 'file' || field.type === 'reset' || field.type === 'submit' || field.type === 'button') continue
-
-    if ((field.type !== 'checkbox' && field.type !== 'radio') || field.checked) {
-      serialized += '&' + encodeURIComponent(field.name) + "=" + encodeURIComponent(field.value)
-    }
-  }
-
-  return serialized;
-}
-
 window.displayMailChimpStatus = function (data) {
   if (!data.result || !data.msg) return
 
-  var mcStatus = document.querySelector('.mc-status')
+  const mcStatus = document.querySelector('.mc-status')
 
   if (!mcStatus) return
 
   mcStatus.innerHTML = data.msg
 
-  //mcStatus.addAttribute('tabindex', '-1')
   mcStatus.focus()
 
   mcStatus.classList.remove('hidden')
@@ -41,15 +23,32 @@ window.displayMailChimpStatus = function (data) {
   mcStatus.classList.add('mc-success')
 }
 
-var submitMailChimpForm = function (form) {
-  var url = form.getAttribute('action')
+const serialize = function (form) {
+  let serialized = ''
+
+  for (let i = 0; i < form.elements.length; i++) {
+
+    const field = form.elements[i]
+
+    if (!field.name || field.disabled || field.type === 'file' || field.type === 'reset' || field.type === 'submit' || field.type === 'button') continue
+
+    if ((field.type !== 'checkbox' && field.type !== 'radio') || field.checked) {
+      serialized += '&' + encodeURIComponent(field.name) + "=" + encodeURIComponent(field.value)
+    }
+  }
+
+  return serialized;
+}
+
+const submitMailChimpForm = function (form) {
+  let url = form.getAttribute('action')
   url = url.replace('/post?u=', '/post-json?u=')
   url += serialize(form) + '&c=displayMailChimpStatus'
 
-  var script = window.document.createElement('script')
+  const script = window.document.createElement('script')
   script.src = url
 
-  var ref = window.document.getElementsByTagName('script')[0]
+  const ref = window.document.getElementsByTagName('script')[0]
   ref.parentNode.insertBefore(script, ref)
 
   script.onload = function () {
